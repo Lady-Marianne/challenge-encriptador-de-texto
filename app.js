54b2fc9add303c203f6e9253f7cc4f341003e502
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-08-12 15:19:35 -0300

    Validar texto.

--- app.js
+++ app.js
@@ -13,12 +13,24 @@ La letra "o" es convertida para "ober".
 La letra "u" es convertida para "ufat". */
 
 function botonEncriptar() {
-    const textoEncriptado = encriptar(areaTexto.value);
-    mensaje.value = textoEncriptado;
-    areaTexto.value = "";
-    mensaje.style.backgroundImage = "none";
-    document.querySelector('.ocultar').classList.add('hidden'); // Oculta el div 'ocultar'.
-}
+    if (areaTexto.value=="") {
+        var mensajeError = document.getElementById("mensaje-error");
+        mensajeError.textContent = "Por favor ingrese un texto o palabra.";
+        setTimeout(function() {mensajeError.textContent = "";}, 5000);
+    }
+    else if (/[^a-z/s]/.test(areaTexto.value)) {
+        var mensajeError = document.getElementById("mensaje-error");
+        mensajeError.textContent = "Por favor, ingrese texto simple sin caracteres especiales ni tildes.";
+        setTimeout(function() {mensajeError.textContent = "";}, 5000);
+    }
+    else {
+        const textoEncriptado = encriptar(areaTexto.value);
+        mensaje.value = textoEncriptado;
+        areaTexto.value = "";
+        mensaje.style.backgroundImage = "none";
+        document.querySelector('.ocultar').classList.add('hidden'); // Oculta el div 'ocultar'.
+    }  
+ }
 
 function encriptar(cadenaEncriptada) {
     let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"], ["o", "ober"], ["u", "ufat"]];
@@ -32,10 +44,23 @@ function encriptar(cadenaEncriptada) {
 }
 
 function botonDesencriptar() {
-    const textoEncriptado = desencriptar(areaTexto.value);
-    mensaje.value = textoEncriptado;
-    areaTexto.value = "";
-    document.querySelector('.ocultar').classList.add('hidden'); // Oculta el div 'ocultar'.
+    if (areaTexto.value=="") {
+        var mensajeError = document.getElementById("mensaje-error");
+        mensajeError.textContent = "Por favor ingrese un texto o palabra.";
+        setTimeout(function() {mensajeError.textContent = "";}, 5000);
+    }
+    else if (/[^a-z/s]/.test(areaTexto.value)) {
+        var mensajeError = document.getElementById("mensaje-error");
+        mensajeError.textContent = "Por favor, ingrese texto simple sin caracteres especiales ni tildes.";
+        setTimeout(function() {mensajeError.textContent = "";}, 5000);
+        return;
+    }
+    else {
+        const textoEncriptado = desencriptar(areaTexto.value);
+        mensaje.value = textoEncriptado;
+        areaTexto.value = "";
+        document.querySelector('.ocultar').classList.add('hidden'); // Oculta el div 'ocultar'.
+    }
 }
 
 function desencriptar(cadenaDesencriptada) {
@@ -60,5 +85,3 @@ function copiar() {
     mensajeConfirmacion.textContent = "Texto copiado al portapapeles";
     setTimeout(function() {mensajeConfirmacion.textContent = "";}, 5000);
 }
-
-window.onload = function() {document.querySelector('.ocultar').classList.remove('hidden');};
